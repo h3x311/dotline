78cbd0fda9cf34f697b47d38c15dba0699d7fb22
h3x311 <soberzml@tutanota.com>
2024-09-06 02:19:15 +0800

    magic

--- src/utils/getSortedThoughts.tsx
+++ src/utils/getSortedThoughts.tsx
@@ -5,8 +5,8 @@ const getSortedThoughts = (thoughts: CollectionEntry<"thoughts">[]) =>
     .filter(({ data }) => !data.draft)
     .sort(
       (a, b) =>
-        Math.floor(new Date(b.data.date).getTime() / 1000) -
-        Math.floor(new Date(a.data.date).getTime() / 1000)
+        Math.floor(new Date(b.data.modDatetime).getTime() / 1000) -
+        Math.floor(new Date(a.data.modDatetime).getTime() / 1000)
     );
 
 export default getSortedThoughts;
